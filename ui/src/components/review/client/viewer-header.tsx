7a190b4613a44db063f693c05197f9cda5788af6
wnhlee <2wheeh@gmail.com>
2024-03-12 21:27:21 +0900

    feat: implement ViewerHeader

---
+++ ui/src/components/review/client/viewer-header.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Link from 'next/link';
+
+import { BoardHeader } from '@/components/common/server/board-header';
+import Text from '@/components/common/server/text';
+import { UpdateReviewButton } from '@/components/review/client/update-review-button';
+import { useViewerHeader } from '@/hooks/review/use-viewer-header';
+
+export function ViewerHeader() {
+  const { isEditable, isAuthorized, handleClickCancel, handleClickEdit } = useViewerHeader();
+
+  return (
+    <BoardHeader>
+      {isEditable ? (
+        <button onClick={handleClickCancel} className="rounded-full border bg-white px-2 py-1">
+          수정취소
+        </button>
+      ) : (
+        <Link href="/review" className="rounded-full border bg-white px-2 py-1">
+          <Text>목록으로</Text>
+        </Link>
+      )}
+
+      {isAuthorized &&
+        (isEditable ? (
+          <UpdateReviewButton />
+        ) : (
+          <button onClick={handleClickEdit} className="rounded-full border bg-white px-2 py-1">
+            수정하기
+          </button>
+        ))}
+    </BoardHeader>
+  );
+}
---
+++ ui/src/hooks/review/use-viewer-header.ts
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+import type { EditorState } from 'lexical';
+
+import { useAuth } from '@/context/auth/auth-context';
+import { useEditorRef } from '@/context/review/editor-ref-context';
+import { useReview } from '@/context/review/review-context';
+import { useEditable } from '@/context/review/editable-context';
+
+export function useViewerHeader() {
+  const { user } = useAuth();
+  const { isEditable, authorId, setIsEditable } = useEditable() ?? {};
+
+  const isAuthorized =
+    user?.accessLevel === 'ADMIN' || user?.accessLevel === 'DEVELOPER' || user?.id === authorId;
+
+  const [savedState, setSavedState] = useState<{
+    title: string;
+    movieName: string;
+    editorState: EditorState;
+  }>();
+
+  const { setMovieName, setTitle, title, movieName } = useReview();
+
+  const { editorRef } = useEditorRef() ?? {};
+  const editor = editorRef?.current;
+
+  const handleClickCancel = () => {
+    if (savedState && editor && setIsEditable) {
+      setMovieName(savedState.movieName);
+      setTitle(savedState.title);
+      editor.setEditorState(savedState.editorState);
+
+      setIsEditable(false);
+    }
+  };
+
+  const handleClickEdit = () => {
+    if (editor && setIsEditable) {
+      setSavedState({ title, movieName, editorState: editor.getEditorState() });
+
+      setIsEditable(true);
+    }
+  };
+
+  return { handleClickCancel, handleClickEdit, isAuthorized, isEditable };
+}
