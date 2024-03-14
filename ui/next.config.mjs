221d5ae497887f6ce8d1b12ceba70ffa991d0093
wnhlee <2wheeh@gmail.com>
2024-03-14 11:39:46 +0900

    feat: add dockerfile for standalone

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -5,6 +5,7 @@ const withBundleAnalzyer = bundleAnalzer({ enabled: process.env.ANALYZE === 'tru
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: () => [{ source: '/api/:path*', destination: `${process.env.BACKEND_URL}/api/:path*` }],
+  output: 'standalone',
 };
 
 export default withBundleAnalzyer(nextConfig);
