e0fb6cc84ae4cfd84d67e9a2218ebd690d06e5a7
wnhlee <2wheeh@gmail.com>
2024-03-20 19:11:29 +0900

    feat: replace workaround EditorRefPlugin with lexicals

--- ui/src/editor/plugins/index.tsx
+++ ui/src/editor/plugins/index.tsx
@@ -3,8 +3,8 @@ import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 
 import { useEditorRef } from '@/context/editor/editor-ref-context';
-import { EditorRefPlugin } from '@/editor/plugins/editor-ref';
 import { MarkdownShortcutPlugin } from '@/editor/plugins/markdown-shorcut';
+import { EditorRefPlugin } from '@lexical/react/LexicalEditorRefPlugin';
 
 function Placeholder() {
   return <div className="placeholder">Begin writing your review...</div>;
